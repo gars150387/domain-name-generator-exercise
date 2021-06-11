c84d48ec4b744bc8a597b05d1ff66ead0fefb70f
gars150387 <gar.santeliz@gmail.com>
2021-06-11 22:53:48 +0000

    adding space to names

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ for (i=0;i<pronoun.length;i++){
     for (j=0; j<adj.length; j++){
 //       console.log(pronoun[i]+adj[j])
       for (k=0; k<noun.length;k++){
-        console.log(pronoun[i]+adj[j]+noun[k])
+        console.log(pronoun[i]+'_'+adj[j]+'_'+noun[k])
       }
     }
 }
