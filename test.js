544863732ec45c742373e11283657382ed9f722d
gars150387 <gar.santeliz@gmail.com>
2021-06-12 00:23:54 +0000

    connecting with html

--- test.js
+++ test.js
@@ -1,13 +1,25 @@
-let pronoun = ['the','our'];
-let adj = ['great', 'big' ];
-let noun = ['jogger','racoon'];
-
-for (i=0;i<pronoun.length;i++){
-//     console.log(pronoun[i])
-    for (j=0; j<adj.length; j++){
-//       console.log(pronoun[i]+adj[j])
-      for (k=0; k<noun.length;k++){
-        console.log(pronoun[i]+'_'+adj[j]+'_'+noun[k])
-      }
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let names = [];
+function domain() {
+    for (i = 0; i < pronoun.length; i++) {
+        //     console.log(pronoun[i])
+        for (j = 0; j < adj.length; j++) {
+            //       console.log(pronoun[i]+adj[j])
+            for (k = 0; k < noun.length; k++) {
+                console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+                // names.push(domain());
+            }
+        }
     }
+}
+// domain()
+
+let randomName = domain();
+
+// document.getElementById('myBtn').onclick = myFunction;
+
+function myFunction() {
+    document.getElementById('demo').innerHTML = randomName;
 }
