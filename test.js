226e6fd8fa996a7acc8f691ad99b440505da2a30
gars150387 <gar.santeliz@gmail.com>
2021-06-17 00:10:44 +0000

    origin

--- test.js
+++ test.js
@@ -1,25 +1,20 @@
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
-let names = [];
+
 function domain() {
+    let pronoun = ['the', 'our'];
+    let adj = ['great', 'big'];
+    let noun = ['jogger', 'racoon'];
+    let names = [];
+
     for (i = 0; i < pronoun.length; i++) {
         //     console.log(pronoun[i])
         for (j = 0; j < adj.length; j++) {
             //       console.log(pronoun[i]+adj[j])
             for (k = 0; k < noun.length; k++) {
-                console.log(pronoun[i] + adj[j] + noun[k] + ".com");
-                // names.push(domain());
+                // console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+                names.push(pronoun[i] + adj[j] + noun[k] + ".com<br>")
+                
             }
         }
-    }
-}
-// domain()
+    } document.getElementById('demo').innerHTML = names
 
-let randomName = domain();
-
-// document.getElementById('myBtn').onclick = myFunction;
-
-function myFunction() {
-    document.getElementById('demo').innerHTML = randomName;
 }
